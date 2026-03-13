d5b116c37d70f9c9a3df7abf0ed100646a4721c9
Corentin Renard <corentinfox08@gmail.com>
2026-03-13 14:59:30 +0100

    fix: use Auth type directly in getAvatarUrl

--- src/common/getAvatarUrl.ts
+++ src/common/getAvatarUrl.ts
@@ -1,7 +1,7 @@
 const anonymousAvatar = require('/assets/images/anonymous.webp');
 const defaultAvatar = require('/assets/images/default_avatar.webp');
 
-const getAvatarUrl = (auth: Profile['auth']): string => {
+const getAvatarUrl = (auth: Auth | null): string => {
     if (!auth) return `url('${anonymousAvatar}')`;
     return auth.user.avatar ? `url('${auth.user.avatar}')` : `url('${defaultAvatar}')`;
 };
